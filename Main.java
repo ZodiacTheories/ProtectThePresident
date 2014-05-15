d5d41bcdaf36053bc128435240622f1e61f90606
ZodiacTheories <thesamster8games@gmail.com>
2014-05-15 21:30:47 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package me.thesamster8.protectthepresident;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+
+}
