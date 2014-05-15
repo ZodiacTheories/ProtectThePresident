73a126ed7397d29cfe1f093bf00c3c8fa0427a9f
reider45 <reid.campolong1@gmail.com>
2014-05-15 14:55:21 -0400

    First Commit

--- src/me/ptp/ProtectThePresident/Main.java
+++ src/me/ptp/ProtectThePresident/Main.java
@@ -4,9 +4,17 @@ public class Main {
 	
 	public void onEnable(){
 		
+		System.out.println("===============");
+		System.out.println("=[PtP] Enabled=");
+		System.out.println("===============");
+		
 	}
 	
 	public void onDisable(){
+		
+		System.out.println("================");
+		System.out.println("=[PtP] Disabled=");
+		System.out.println("================");
 		
 	}
 
