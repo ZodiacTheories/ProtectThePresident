8a5570783c3303fc30f49f05d1a02390e2e5a6c2
reider45 <reid.campolong1@gmail.com>
2014-05-16 10:45:23 -0400

    GUI Creation on startup
    You need to create a constructor in the GUI class still.

--- src/me/ptp/ProtectThePresident/Main.java
+++ src/me/ptp/ProtectThePresident/Main.java
@@ -4,7 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	public GUI gui = new GUI(this);
+	
 	public void onEnable(){
+		
+		gui.createGui();
 		
 		System.out.println("===============");
 		System.out.println("=[PtP] Enabled=");
